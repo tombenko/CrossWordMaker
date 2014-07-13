5ece26ae87695a75c27492efaca9e6f2a0ff215f
Thomas Benko <tombenko@gmail.com>
2014-07-14 00:06:53 +0200

    New methods: 	drawBorder 	drawLetter 	drawNumber

--- WorkingPanel.java
+++ WorkingPanel.java
@@ -12,12 +12,14 @@ class WorkingPanel extends JPanel{
 	 * In this class do we draw the crossword. The metrics still
 	 * hardcoded, I want first to have the editing works. Parts of the
 	 * class are from zetcode.com (or .org, I always forget it...) in
-	 * hope they don't mind it.
+	 * hope they don't mind.
 	 * */
 	
 	private CrossWord cw;
 	private Dimension size;
-	private Point cursorPosition = new Point(0, 0);;
+	private Point cursorPosition = new Point(0, 0);
+	enum Orientation {HORIZONTAL, VERTICAL};
+	Orientation orientation = Orientation.HORIZONTAL;
 	
 	WorkingPanel(int w, int h){
 		cw = new CrossWord(w, h);
@@ -28,7 +30,6 @@ class WorkingPanel extends JPanel{
 	public void doDrawing(Graphics g){
 		
 		Graphics2D painter = (Graphics2D) g;
-		int i,j;
 		
 		// Some preparations...
 		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
@@ -38,35 +39,20 @@ class WorkingPanel extends JPanel{
 		painter.setRenderingHints(rh);
 		painter.setFont(new Font("Monospaced", Font.PLAIN, 16));
 		
-		// Now paint the net.
-		for(i = 0; i <= size.width; i++){
-			painter.drawLine(i * 20, 0, i * 20, size.height * 20);
-		}
+		//Let's paint the puzzle. This is done by some private methods
+		//for every square. Maybe it would be better to take into a very
+		//new class...
 		
-		for(i = 0; i <= size.height; i++){
-			painter.drawLine(0, i * 20, size.width * 20, i * 20);
-		}
-		
-		// At last we draw the contents of the sqares.
-		painter.setColor(Color.BLACK);
-		
-		for(i = 0; i < size.width; i++){
-			for(j = 0; j < size.height; j++){
-				//Somewhere here must be the bold lines drawing. Maybe...
-				if( (i == cursorPosition.x) && (j == cursorPosition.y) ){
-					painter.setColor(Color.RED);
-				}
-				painter.drawString(cw.getLetter(i,j), i * 20 + 4, j * 20 + 16);
-				painter.setColor(Color.BLACK);
-				if(cw.getLetter(i, j).equals(".")){
-					// Dot marks the black squares. It is because I plan
-					// to export the whole puzzle into the cwpuzzle
-					// LaTeX package's format.
-					painter.fillRect(i * 20, j * 20, 20, 20);
+		for(int i = 0; i < size.width; i++){
+			for(int j = 0; j < size.height; j++){
+				Point whichSquare = new Point(i, j);
+				drawBorders(painter, whichSquare);
+				drawLetter(painter, whichSquare, cw.getLetter(whichSquare.x, whichSquare.y));
+				if(cw.getNumber(whichSquare.x, whichSquare.y) != 0){
+					drawNumber(painter, whichSquare, cw.getNumber(whichSquare.x, whichSquare.y));
 				}
 			}
 		}
-		
 	}
 	
 	public void paintComponent(Graphics g){
@@ -98,7 +84,48 @@ class WorkingPanel extends JPanel{
 	
 	public void setLetter(char c){
 		cw.setLetter(c, cursorPosition.x, cursorPosition.y);
+		if(orientation == Orientation.VERTICAL){
+			cursorPosition.y++;
+		} else {
+			cursorPosition.x++;
+		}
 		repaint();
+	}
+	
+	private void drawBorders(Graphics2D g, Point position){
+		/*
+		 * Here we draw the borders as a gray rectangle. The bold lines
+		 * around the square will be drawn here too. But first I ensure
+		 * the method works as I expected. The size of the rectangle is
+		 * hardcoded yet by the same cause.
+		 * */
+		g.setColor(Color.GRAY);
+		g.drawRect(position.x * 20, position.y * 20, 20, 20);
+	}
+	
+	private void drawLetter(Graphics2D g, Point position, String letter){
+		if(position.equals(cursorPosition)){
+			g.setColor(Color.RED);
+		} else {
+			g.setColor(Color.BLACK);
+		}
+		g.drawString(letter, position.x * 20 + 4, position.y * 20 + 16);
+		if(letter.equals(".")){
+			g.fillRect(position.x * 20, position.y * 20, 20, 20);
+		}
+	}
+	
+	private void drawNumber(Graphics2D g, Point position, int number){
+		g.setFont(new Font("Monospaced", Font.PLAIN, 6));
+		g.drawString(Integer.toString(number), position.x * 20 + 2, position.y * 20 + 6);
+	}
+	
+	public void toggleOrientation(){
+		if(orientation == Orientation.VERTICAL){
+			orientation = Orientation.HORIZONTAL;
+		} else {
+			orientation = Orientation.VERTICAL;
+		}
 	}
 
 }
