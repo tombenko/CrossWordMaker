a43f5b0eeb10c2e6979d66e9e2a229530f850621
Thomas Benko <tombenko@gmail.com>
2014-07-16 16:37:10 +0200

    hanges because of the WorkingPanle changes.

--- WorkingWindow.java
+++ WorkingWindow.java
@@ -6,23 +6,25 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.GridLayout;
+import java.awt.Point;
 
 class WorkingWindow extends JFrame{
-	/*
+	/**
 	 * There is a lot of work to do, this is a very poor main window.
 	 * For example there is a need for a menubar, saving and restoring
 	 * puzzles, handling mouse (maybe this would be easy...) and finding
 	 * out, how to set the bold lines in a square. And the
 	 * most important: sizing the cossword.
-	 * */
+	 * **/
 	
 	//Fields
 	
-	private WorkingPanel wp = new WorkingPanel(21,13);
+	private WorkingPanel wp;
 	
 	//Constructors
 	
 	WorkingWindow(String title){
+		wp = new WorkingPanel(new CrossWord(21,13));
 		initUI(title);
 	}
 	
@@ -37,23 +39,32 @@ class WorkingWindow extends JFrame{
 				public void keyPressed(KeyEvent e){
 					switch(e.getKeyCode()){
 						case KeyEvent.VK_LEFT:{
-							wp.setCursorPos(--wp.getCursorPos().x,wp.getCursorPos().y);
+							wp.setCursorPos(new Point(--wp.getCursorPos().x,wp.getCursorPos().y));
 							break;
 						}
 						case KeyEvent.VK_RIGHT:{
-							wp.setCursorPos(++wp.getCursorPos().x,wp.getCursorPos().y);
+							wp.setCursorPos(new Point(++wp.getCursorPos().x,wp.getCursorPos().y));
 							break;
 						}
 						case KeyEvent.VK_UP:{
-							wp.setCursorPos(wp.getCursorPos().x,--wp.getCursorPos().y);
+							wp.setCursorPos(new Point(wp.getCursorPos().x,--wp.getCursorPos().y));
 							break;
 						}
 						case KeyEvent.VK_DOWN:{
-							wp.setCursorPos(wp.getCursorPos().x,++wp.getCursorPos().y);
+							wp.setCursorPos(new Point(wp.getCursorPos().x,++wp.getCursorPos().y));
+							break;
+						}
+						case KeyEvent.VK_DELETE: {
+							wp.setLetter(' ');
+							break;
+						}
+						case KeyEvent.VK_SPACE: {
+							wp.toggleOrientation();
 							break;
 						}
 						default:{
 							wp.setLetter(e.getKeyChar());
+							wp.stepCursor();
 							break;
 						}
 					}
@@ -90,7 +101,16 @@ class WorkingWindow extends JFrame{
 				public void mouseClicked(MouseEvent e){
 					
 					if(e.getButton() == MouseEvent.BUTTON1){
-						wp.setCursorPos(e.getX() / 20, e.getY() / 20 - 1);
+						wp.setCursorPos(new Point(e.getX() / wp.getMetric(), e.getY() / wp.getMetric()));
+					}
+					if(e.getButton() == MouseEvent.BUTTON2){
+						if( (e.getX() % wp.getMetric()) > 15){
+							wp.toggleSideLine(Square.RIGHT, new Point(e.getX() / wp.getMetric(), e.getY() / wp.getMetric()));
+						}
+						if( (e.getY() % wp.getMetric()) > 15){
+							wp.toggleSideLine(Square.BOTTOM, new Point(e.getX() / wp.getMetric(), e.getY() / wp.getMetric()));
+						}
+						wp.repaint();
 					}
 					
 				}
