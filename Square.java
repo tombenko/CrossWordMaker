f3285fd9a3037ea0b99cdff865de4e0e064cd818
Thomas Benko <tombenko@gmail.com>
2014-07-17 17:27:07 +0200

    MOdified borderline markers. Dh A

--- Square.java
+++ Square.java
@@ -1,22 +1,24 @@
 import java.lang.Character;
 
 class Square{
-	/*
+	/**
 	 * This class describes a square in a crossword. It has a letter
 	 * written in, possibly a number (with any meaning) and maybe lines
 	 * round of it. Because there can be only one letter in a square,
-	 * it's enough to declare it as char. The numbers are usually under 100,
-	 * but in special cases, eg. giant crosswords can have more, so it
-	 * is good to be int. The border lines are bit-sized informations,
-	 * so the four lines existence can be a bytes first four bits. 
-	 * */
+	 * it's enough to declare it as char. The numbers are usually under
+	 * 100, but in special cases, eg. giant crosswords can have more, so
+	 * it is good to be int. The border lines are just a true/false in-
+	 * formation, therefore they are stored in a boolean array.
+	 * **/
 	
 	//Fields
 	
+	public static final short RIGHT = 0;	//These two values are for
+	public static final short BOTTOM = 1;	//seting the bold sidelines.
+	
 	private char letter;
 	private int number;
-	private byte sideLine;	//The first 4 bits signs which line is drawn.
-							//1 - left, 2 - right, 4 - top, 8 - bottom
+	private boolean[] sideLine = {false, false};
 	
 	//Constructors
 	
@@ -28,7 +30,6 @@ class Square{
 		
 		letter = ' ';
 		number = 0;
-		sideLine = 0;
 		
 	}
 	
@@ -36,7 +37,6 @@ class Square{
 		
 		this.letter = letter;
 		this.number = 0;
-		this.sideLine = 0;
 		
 	}
 	
@@ -46,10 +46,6 @@ class Square{
 		/*
 		 * Sets the letter written into the square. Capital letters are
 		 * more readable, so all the letters changed to this shape.
-		 * 
-		 * TODO:
-		 * Throw exception when the parameter is not a letter. (I take
-		 * space as letter to have empty squares.)
 		 * */
 		
 		this.letter = Character.toUpperCase(letter);
@@ -83,18 +79,21 @@ class Square{
 		
 	}
 	
-	public void toggleSideLine(byte which){
+	public void toggleSideLine(short which){
+		
 		/*
-		 * Here we set the border line. The 'which' parameter has the
-		 * same description, as the SideLine variable. The set and unset
-		 * goes the same way, therefore it is enough to made with XOR.
+		 * The which parameter is described above. For sure we take the
+		 * modulo 2 remains of the given parameter for not slipping out
+		 * of the array bounds.
 		 * */
 		
-		this.sideLine ^= which;
+		which %= 2;
+		
+		this.sideLine[which] ^= true;
 		
 	}
 	
-	public byte getSideLine(){
+	public boolean[] getSideLine(){
 		
 		/*
 		 * Returns the border lines.
