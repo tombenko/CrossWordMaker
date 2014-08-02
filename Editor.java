26dffb8b16b8fc87daf622c935948fb7a73e7f3c
Thomas Benko <tombenko@gmail.com>
2014-08-02 23:47:14 +0200

    The would be Listener implementation to edit the puzzle

--- Editor.java
+++ Editor.java
@@ -4,32 +4,18 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.Point;
 
-class Editor implements Runnable, KeyListener, MouseListener{
+class Editor implements KeyListener, MouseListener{
 	private CrossWord workingInstance;
-	private Point activeSquare;
-	private Communicator communicator;
-	private boolean wasModify = false;
-	private int metric = 20;
+	private Point activeSquare = new Point(0,0);
 	
 	
-	public Editor(CrossWord cw, Communicator communicator){
-		this.communicator = communicator;
+	public Editor(CrossWord cw){
 		workingInstance = cw;
 	}
 	
-	public Editor(CrossWord cw, Point ap, Communicator communicator){
-		this.communicator = communicator;
+	public Editor(CrossWord cw, Point ap){
 		workingInstance = cw;
 		activeSquare = ap;
-	}
-	
-	public void run(){
-		while(true){
-			if(wasModify){
-				communicator.putMessage(workingInstance, activeSquare);
-				wasModify = false;
-			}
-		}
 	}
 	
 	public void keyPressed(KeyEvent e){
@@ -42,6 +28,39 @@ class Editor implements Runnable, KeyListener, MouseListener{
 	
 	public void keyTyped(KeyEvent e){
 		
+		System.out.println("key");
+		
+		switch(e.getKeyCode()){
+			case KeyEvent.VK_UP:{
+				if(activeSquare.y > 0){
+					activeSquare.translate(0,-1);
+				}
+				break;
+			}
+			case KeyEvent.VK_DOWN:{
+				if(activeSquare.y < workingInstance.getSize().height){
+					activeSquare.translate(0,1);
+				}
+				break;
+			}
+			case KeyEvent.VK_LEFT:{
+				if(activeSquare.x < workingInstance.getSize().width){
+					activeSquare.translate(-1,0);
+				}
+				break;
+			}
+			case KeyEvent.VK_RIGHT:{
+				if(activeSquare.x > 0){
+					activeSquare.translate(1,0);
+				}
+				break;
+			}
+			default:{
+				System.out.println(Character.toString(e.getKeyChar()));
+				workingInstance.setLetter(e.getKeyChar(), activeSquare);
+			}
+		}
+		notify();
 	}
 	
 	public void mouseEntered(MouseEvent e){
