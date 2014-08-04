553ef664942479e0f56cf40d24f522c387e38ea7
Thomas Benko <tombenko@gmail.com>
2014-08-04 21:21:35 +0200

    Now it is better, cleaner, drier. And still ugly...

--- WorkingWindow.java
+++ WorkingWindow.java
@@ -19,8 +19,10 @@ class WorkingWindow extends JFrame{
 	 * most important: sizing the cossword.
 	 * **/
 	
-	private WorkingPanel workArea;		//Where we will see what we did.
-	private CrossWord workInstance;		//It is the crossword we plan to create.
+	private WorkingPanel workArea;
+	private CrossWord workInstance;
+	private Point activeSquare = new Point(0,0);
+	private Point writeDirection = new Point(1,0);
 	
 	public WorkingWindow(String title){
 		workInstance = new CrossWord(new Dimension(15, 17));
@@ -37,6 +39,80 @@ class WorkingWindow extends JFrame{
 		setTitle(title);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		add(workArea);
+		
+		//It feels like bad coding to me, but it works. Planning to move
+		//the inner class to an outer (but functioning) class.
+		addKeyListener(new KeyListener(){
+			public void keyPressed(KeyEvent e){
+				switch(e.getKeyCode()){
+					//Moving the cursor. Mostly invisible...
+					case KeyEvent.VK_UP:{
+						if(activeSquare.y > 0){
+							activeSquare.translate(0, -1);
+						}
+						break;
+					}
+					case KeyEvent.VK_DOWN:{
+						if(activeSquare.y < (workInstance.getSize().height - 1)){
+							activeSquare.translate(0, 1);
+						}
+						break;
+					}
+					case KeyEvent.VK_LEFT:{
+						if(activeSquare.x > 0){
+							activeSquare.translate(-1, 0);
+						}
+						break;
+					}
+					case KeyEvent.VK_RIGHT:{
+						if(activeSquare.x < (workInstance.getSize().width - 1)){
+							activeSquare.translate(1, 0);
+						}
+						break;
+					}
+					//Changing the direction
+					case KeyEvent.VK_SPACE:{
+						int temp = writeDirection.y;
+						writeDirection.y = writeDirection.x;
+						writeDirection.x = temp;
+						break;
+					}
+					//Toggling the borderlines
+					case KeyEvent.VK_COMMA:{
+						workInstance.toggleSideLine(Square.BOTTOM, activeSquare);
+						break;
+					}
+					case KeyEvent.VK_MINUS:{
+						workInstance.toggleSideLine(Square.RIGHT, activeSquare);
+						break;
+					}
+					//Writing the content
+					default:{
+						if(Character.isLetter(e.getKeyChar()) || (e.getKeyChar() == '.') ){
+							workInstance.setLetter(e.getKeyChar(), activeSquare);
+							activeSquare.translate(writeDirection.x, writeDirection.y);
+						}
+						if(activeSquare.x >= workInstance.getSize().width){
+							activeSquare.x = workInstance.getSize().width - 1;
+						}
+						if(activeSquare.y >= workInstance.getSize().height){
+							activeSquare.y = workInstance.getSize().height - 1;
+						}
+						
+						break;
+					}
+				}
+				workArea.setDrawing(workInstance, activeSquare);
+			}
+			
+			public void keyTyped(KeyEvent e){
+				
+			}
+			
+			public void keyReleased(KeyEvent e){
+				
+			}
+		});
 		pack();
 		setResizable(false);
 	}
