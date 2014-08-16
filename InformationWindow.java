0c16e3962a5108bb4b8d08343d917ed20d1b5ecc
Thomas Benko <tombenko@gmail.com>
2014-08-16 21:15:12 +0200

    A new class for the information windows

---
+++ InformationWindow.java
@@ -0,0 +1,33 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridLayout;
+
+class InformationWindow extends JFrame{
+	
+	public InformationWindow(String title, String text){
+		initWindow(title, text);
+	}
+	
+	private void initWindow(String title, String text){
+		JButton button = new JButton("OK");
+		setLayout(new GridLayout(2,1));
+		add(new JLabel(text));
+		add(button);
+		button.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				dispose();
+			}
+		});
+		setTitle(title);
+		pack();
+		setVisible(true);
+	}
+	
+	@Override
+	public void dispose(){
+		super.dispose();
+	}
+}
