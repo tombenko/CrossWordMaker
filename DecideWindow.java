7063b0f75ea38bc2c3784dfbed66be472f5efb80
tombenko <tombenko@gmail.com>
2014-11-13 00:17:22 +0100

    Delete DecideWindow.java

--- DecideWindow.java
+++
@@ -1,8 +0,0 @@
-package DialogWindows.DecideWindow;
-
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridLayout;
